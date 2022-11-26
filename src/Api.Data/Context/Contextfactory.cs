30a922c9c0804bc553f868c6392b19203fd6d22f
Cristiano Campos <cristiano.souzac@hotmail.com.br>
2022-11-25 23:08:00 -0300

    Created class context  entity framework

---
+++ src/Api.Data/Context/Contextfactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context;
+
+public class Contextfactory : IDesignTimeDbContextFactory<MyContext>
+{
+    public MyContext CreateDbContext(string[] args)
+    {
+        //Usado para Criar as Migrações
+        var connectionString = "Server=HOME-CRISTIANO;Database=curso_aspnetcore_api;User Id=sa;Password=3271;";
+        //var connectionString = "Server=.\\SQLEXPRESS2017;Database=dbAPI;User Id=sa;Password=3271";
+        var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+        //optionsBuilder.UseMySql (connectionString);
+        optionsBuilder.UseSqlServer(connectionString);
+        return new MyContext(optionsBuilder.Options);
+    }
+}
---
+++ src/Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.Domain.Entities;
+
+public class UserEntity : BaseEntity
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
