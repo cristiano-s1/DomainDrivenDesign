f3a03a14674fa2b9e0d20b76d0398c28df8c3acd
Cristiano Campos <cristiano.souzac@hotmail.com.br>
2022-11-27 14:13:48 -0300

    Added  User services and Users controllers

---
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,24 @@
+﻿using Api.Data.Context;
+using Api.Data.Repository;
+using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependencyInjection;
+
+public class ConfigureRepository
+{
+    public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+        //serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+
+        //// serviceCollection.AddDbContext<MyContext> (
+        ////     options => options.UseMySql ("Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123")
+        //// );
+
+        serviceCollection.AddDbContext<MyContext>(
+            options => options.UseSqlServer("Server=HOME-CRISTIANO;Database=curso_aspnetcore_api;User Id=sa;Password=3271")
+        );
+    }
+}
---
+++ src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,14 @@
+﻿using Api.Service.Services;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependencyInjection;
+
+public class ConfigureService
+{
+    public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddTransient<IUserService, UserService>();
+        //serviceCollection.AddTransient<ILoginService, LoginService>();
+    }
+}
